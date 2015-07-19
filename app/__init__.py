7734d519bc862ce26b65f4a9dcc8bb8343a43b2b
Sean Beck <seanmckaybeck@gmail.com>
2015-07-19 19:38:39 -0400

    killed flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
