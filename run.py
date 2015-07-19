b39a2240d4d6d6255532a35c0a3361403c637ff5
Sean Beck <seanmckaybeck@gmail.com>
2015-07-19 09:01:01 -0400

    Added argparse to run.py added github button to base.html

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
+import argparse
+
 from app import app
-app.run(debug=True)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', action='store_true', help='Run this application in debug mode')
+parser.add_argument('-p', type=int, default=8000, help='The port to run this on')
+parser.add_argument('-H', type=str, default='127.0.0.1', help='The interface to run this application on')
+args = parser.parse_args()
+
+app.run(host=args.H, port=args.p, debug=args.d)
